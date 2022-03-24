4d0d9bf9e8c19e2ab4d520ec90de6d8f7ba85231
julio.moro <julio.moro@inetum.com>
2022-03-24 11:53:47 +0100

    Practica_3_-Spring-boot-DemoWeb

---
+++ DemoWebJulio/src/main/java/es/demoJulio/DemoWebJulioApplication.java
@@ -0,0 +1,13 @@
+package es.demoJulio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebJulioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebJulioApplication.class, args);
+	}
+
+}
---
+++ DemoWebJulio/src/test/java/es/demoJulio/DemoWebJulioApplicationTests.java
@@ -0,0 +1,13 @@
+package es.demoJulio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebJulioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
